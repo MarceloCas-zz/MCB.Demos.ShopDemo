802ebcb022a79a87d88d40dd186297160babc49e
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-27 23:25:06 -0300

    feat: add customer microservices projects

---
+++ tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Test/DummyUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Test
+{
+    public class DummyUnitTest
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
