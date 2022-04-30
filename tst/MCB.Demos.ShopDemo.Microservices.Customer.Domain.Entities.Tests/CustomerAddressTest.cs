490d1c38fc964fb3422d784376d608c4d5263c3f
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-29 21:01:39 -0300

    test: add CustomerAddress Test for RegisterNew operation

--- tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/CustomerAddressTest.cs
+++ tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/CustomerAddressTest.cs
@@ -36,5 +36,56 @@ namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests
             customerAddress.CustomerAddressType.Should().Be(default);
             customerAddress.AddressValueObject.Should().Be(default(AddressValueObject));
         }
+
+        [Fact]
+        public void CustomerAddress_Should_RegisterNew()
+        {
+            // Arrange
+            var street = "dummy street";
+            var number = "dummy number";
+            var city = "dummy city";
+            var state = "dummy state";
+            var country = "dummy country";
+            var zipCode = "dummy zipCode";
+
+            foreach (var customerAddressType in Enum.GetValues<CustomerAddressType>())
+            {
+                var addressValueObject = new AddressValueObject(
+                    street,
+                    number,
+                    city,
+                    state,
+                    country,
+                    zipCode
+                );
+
+                // Act
+                var customerAddress = new CustomerAddress().RegisterNew(
+                    _fixture.TenantId,
+                    customerAddressType,
+                    addressValueObject,
+                    _fixture.ExecutionUser,
+                    _fixture.SourcePlatform
+                );
+
+                // Assert
+                customerAddress.Id.Should().NotBe(Guid.Empty);
+                customerAddress.TenantId.Should().Be(_fixture.TenantId);
+
+                customerAddress.AuditableInfo.Should().NotBeNull();
+                customerAddress.AuditableInfo.CreatedAt.Should().BeAfter(default);
+                customerAddress.AuditableInfo.CreatedAt.Should().BeBefore(DateTimeOffset.UtcNow);
+                customerAddress.AuditableInfo.CreatedBy.Should().Be(_fixture.ExecutionUser);
+                customerAddress.AuditableInfo.UpdatedAt.Should().BeNull();
+                customerAddress.AuditableInfo.UpdatedBy.Should().BeNull();
+                customerAddress.AuditableInfo.SourcePlatform.Should().Be(_fixture.SourcePlatform);
+
+                customerAddress.RegistryVersion.Should().BeAfter(default);
+                customerAddress.RegistryVersion.Should().BeBefore(DateTimeOffset.UtcNow);
+
+                customerAddress.CustomerAddressType.Should().Be(customerAddressType);
+                customerAddress.AddressValueObject.Should().Be(addressValueObject);
+            }
+        }
     }
 }
