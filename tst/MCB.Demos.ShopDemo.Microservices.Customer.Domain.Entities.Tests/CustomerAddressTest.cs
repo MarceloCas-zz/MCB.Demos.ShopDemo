bb3e18ef913b1341e7b6e217f963f7a1b614b614
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-30 20:28:15 -0300

    test: add CustomerAddress_Should_ChangeAddress

--- tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/CustomerAddressTest.cs
+++ tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/CustomerAddressTest.cs
@@ -121,5 +121,53 @@ namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests
             customerAddress.CustomerAddressType.Should().NotBe(initialCustomerAddressType);
             customerAddress.CustomerAddressType.Should().Be(newCustomerAddressType);
         }
+
+        [Fact]
+        public void CustomerAddress_Should_ChangeAddress()
+        {
+            // Arrange
+            var originalGetDataFuncion = DateTimeProvider.GetDateCustomFunction;
+            DateTimeProvider.GetDateCustomFunction = () => originalGetDataFuncion().AddDays(-1);
+            var customerAddress = DefaultFixture.GenerateNewCustomerAddress(existingCustomerAddress: DefaultFixture.GenerateNewAddressValueObject());
+            DateTimeProvider.GetDateCustomFunction = originalGetDataFuncion;
+
+            var newCustomerAddress = DefaultFixture.GenerateNewAddressValueObject();
+
+            var initialId = customerAddress.Id;
+            var initialTenantId = customerAddress.TenantId;
+
+            var initialCreatedAt = customerAddress.AuditableInfo.CreatedAt;
+            var initialCreatedBy = customerAddress.AuditableInfo.CreatedBy;
+            var initialUpdatedAt = customerAddress.AuditableInfo.UpdatedAt;
+            var initialUpdatedBy = customerAddress.AuditableInfo.UpdatedBy;
+            var initialSourcePlatform = customerAddress.AuditableInfo.SourcePlatform;
+            var initialRegistryVersion = customerAddress.RegistryVersion;
+            var initialCustomerAddressType = customerAddress.CustomerAddressType;
+
+            // Act
+            customerAddress.ChangeAddress(newCustomerAddress, _fixture.ExecutionUser, _fixture.SourcePlatform);
+
+            // Assert
+            customerAddress.Id.Should().Be(initialId);
+            customerAddress.TenantId.Should().Be(initialTenantId);
+
+            customerAddress.AuditableInfo.CreatedAt.Should().Be(initialCreatedAt);
+            customerAddress.AuditableInfo.CreatedBy.Should().Be(initialCreatedBy);
+
+            customerAddress.AuditableInfo.UpdatedAt.Should().NotBe(initialUpdatedAt);
+            customerAddress.AuditableInfo.UpdatedAt.Should().Be(DateTimeProvider.GetDate());
+
+            customerAddress.AuditableInfo.UpdatedBy.Should().NotBe(initialUpdatedBy);
+            customerAddress.AuditableInfo.UpdatedBy.Should().Be(_fixture.ExecutionUser);
+
+            customerAddress.AuditableInfo.SourcePlatform.Should().NotBe(initialSourcePlatform);
+            customerAddress.AuditableInfo.SourcePlatform.Should().Be(_fixture.SourcePlatform);
+
+            customerAddress.RegistryVersion.Should().NotBe(initialRegistryVersion);
+            customerAddress.RegistryVersion.Should().Be(DateTimeProvider.GetDate());
+
+            customerAddress.AddressValueObject.Should().NotBe(initialCustomerAddressType);
+            customerAddress.AddressValueObject.Should().Be(newCustomerAddress);
+        }
     }
 }
