c40fd1af19b4c5cbde222f1a9868a76235a4419c
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-05-16 01:32:11 -0300

    test: AddressValueObject_Should_Correctly_Initialized

--- tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/ValueObjectsTests/AddressValueObjectTest.cs
+++ tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/ValueObjectsTests/AddressValueObjectTest.cs
@@ -2,11 +2,6 @@
 using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests.Fixtures;
 using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.ValueObjects;
 using MCB.Tests;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
