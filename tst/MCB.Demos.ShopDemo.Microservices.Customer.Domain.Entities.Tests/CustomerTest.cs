5634fa7ee183c6feff28cccf27a3f33b3aa31356
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-05-16 01:44:51 -0300

    test: add Customer_Should_Correctly_Instanciated

---
+++ tst/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests/CustomerTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests.Fixtures;
+using MCB.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Tests
+{
+    [Collection(nameof(DefaultFixture))]
+    public class CustomerTest
+        : TestBase
+    {
+        // Fields
+        private readonly DefaultFixture _fixture;
+
+        // Constructors
+        public CustomerTest(
+            ITestOutputHelper testOutputHelper,
+            DefaultFixture fixture
+        ) : base(testOutputHelper)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Customer_Should_Correctly_Instanciated()
+        {
+            // Arrange and Act
+            var customer = new Customer();
+
+            // Assert
+            customer.FirstName.Should().BeNullOrEmpty();
+            customer.LastName.Should().BeNullOrEmpty();
+            customer.BirthDate.Should().Be(default);
+            customer.CustomerAddressInfo.Should().NotBeNull();
+        }
+    }
+}
