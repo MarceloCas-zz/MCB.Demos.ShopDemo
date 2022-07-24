51bab153f056808189ed7853ec6aa300a651401e
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-07-23 23:40:23 -0300

    feat: add input validators abstractions

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/Customers/Validators/Interfaces/IAddNewCustomerAddressInputShouldBeValidValidator.cs
@@ -0,0 +1,10 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Inputs;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Validators.Interfaces
+{
+    public interface IAddNewCustomerAddressInputShouldBeValidValidator
+        : IValidator<AddNewCustomerAddressInput>
+    {
+    }
+}
