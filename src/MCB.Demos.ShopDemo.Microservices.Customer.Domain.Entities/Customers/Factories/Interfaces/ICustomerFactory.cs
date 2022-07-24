bfd29a81dfe45118f4d4d66a74952bdebde9791f
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-07-23 21:25:30 -0300

    refact: remove factories

--- src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/Customers/Factories/Interfaces/ICustomerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Factory;
-
-namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Factories.Interfaces
-{
-    public interface ICustomerFactory
-        : IFactory<Customer>
-    {
-    }
-}
