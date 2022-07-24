96122d6f14b30b5f518dbe730337cbac2fe8465a
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-07-24 00:02:50 -0300

    refact: add CustomerAddressInfoSpecification

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/Customers/Specifications/CustomerAddressSpecifications.cs
@@ -0,0 +1,22 @@
+﻿using MCB.Core.Domain.Entities.DomainEntitiesBase.Specifications;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Enums;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Specifications.Interfaces;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.ValueObjects;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Specifications
+{
+    public class CustomerAddressSpecifications
+        : DomainEntitySpecifications,
+        ICustomerAddressSpecifications
+    {
+        // Public Methods
+        public bool CustomerAddressShouldHaveCustomerAddressType(CustomerAddressType customerAddressType)
+        {
+            return Enum.IsDefined(typeof(CustomerAddressType), customerAddressType);
+        }
+        public bool CustomerAddressShouldHaveAddressValueObject(AddressValueObject addressValueObject)
+        {
+            return !addressValueObject.Equals(new AddressValueObject());
+        }
+    }
+}
---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/Customers/Specifications/Interfaces/ICustomerAddressSpecifications.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MCB.Core.Domain.Entities.Abstractions.Specifications.Interfaces;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Enums;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.ValueObjects;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.Customers.Specifications.Interfaces
+{
+    public interface ICustomerAddressSpecifications
+        : IDomainEntitySpecifications
+    {
+        // CustomerAddressType
+        public static readonly string CustomerAddressShouldHaveCustomerAddressTypeErrorCode = nameof(CustomerAddressShouldHaveCustomerAddressTypeErrorCode);
+        public static readonly string CustomerAddressShouldHaveCustomerAddressTypeMessage = nameof(CustomerAddressShouldHaveCustomerAddressTypeMessage);
+        public static readonly Severity CustomerAddressShouldHaveCustomerAddressTypeSeverity = Severity.Error;
+
+        // AddressValueObject
+        public static readonly string CustomerAddressShouldHaveAddressValueObjectErrorCode = nameof(CustomerAddressShouldHaveAddressValueObjectErrorCode);
+        public static readonly string CustomerAddressShouldHaveAddressValueObjectMessage = nameof(CustomerAddressShouldHaveAddressValueObjectMessage);
+        public static readonly Severity CustomerAddressShouldHaveAddressValueObjectSeverity = Severity.Error;
+
+        // Public Methods
+        bool CustomerAddressShouldHaveCustomerAddressType(CustomerAddressType customerAddressType);
+        bool CustomerAddressShouldHaveAddressValueObject(AddressValueObject addressValueObject);
+    }
+}
