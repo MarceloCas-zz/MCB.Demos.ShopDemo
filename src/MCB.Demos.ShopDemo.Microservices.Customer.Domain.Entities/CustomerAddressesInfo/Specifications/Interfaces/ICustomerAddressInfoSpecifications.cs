290bcc6fc51586a9eb740cafcbb9102b835e1ed0
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-08-23 07:13:42 -0300

    refact: change CustomerAddressSpecifications

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddressesInfo/Specifications/Interfaces/ICustomerAddressInfoSpecifications.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MCB.Core.Domain.Entities.Abstractions.Specifications.Interfaces;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddresses;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Specifications.Interfaces
+{
+    public interface ICustomerAddressInfoSpecifications
+        : IDomainEntitySpecifications
+    {
+        // CustomerAddressCollection
+        public static readonly string CustomerAddressInfoShouldHaveCustomerAddressCollectionErrorCode = nameof(CustomerAddressInfoShouldHaveCustomerAddressCollectionErrorCode);
+        public static readonly string CustomerAddressInfoShouldHaveCustomerAddressCollectionMessage = nameof(CustomerAddressInfoShouldHaveCustomerAddressCollectionMessage);
+        public static readonly Severity CustomerAddressInfoShouldHaveCustomerAddressCollectionSeverity = Severity.Error;
+
+        // DefaultShippingAddress
+        public static readonly string CustomerAddressInfoShouldHaveDefaultShippingAddresstErrorCode = nameof(CustomerAddressInfoShouldHaveDefaultShippingAddresstErrorCode);
+        public static readonly string CustomerAddressInfoShouldHaveDefaultShippingAddressMessage = nameof(CustomerAddressInfoShouldHaveDefaultShippingAddressMessage);
+        public static readonly Severity CustomerAddressInfoShouldHaveDefaultShippingAddressSeverity = Severity.Error;
+
+        // Others
+        public static readonly string CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollectionErrorCode = nameof(CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollectionErrorCode);
+        public static readonly string CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollectionMessage = nameof(CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollectionMessage);
+        public static readonly Severity CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollectionSeverity = Severity.Error;
+
+        // Public Methods
+        bool CustomerAddressInfoShouldHaveCustomerAddressCollection(IEnumerable<CustomerAddress> customerAddressCollection);
+        bool CustomerAddressInfoShouldHaveDefaultShippingAddress(CustomerAddress defaultShippingAddress);
+        bool CustomerAddressInfoShouldHaveDefaultShippingAddressInCustomerAddressCollection(CustomerAddress defaultShippingAddress, IEnumerable<CustomerAddress> customerAddressCollection);
+    }
+}
