331d6d7d12c877f665a102102e933ac69f8db7cc
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-08-24 00:25:43 -0300

    feat: add CustomerAddressesInfo validators abstractions

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddressesInfo/Validators/Interfaces/IChangeDefaultCustomerAddressInfoShippingAddressValidator.cs
@@ -0,0 +1,10 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Inputs;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Validators.Interfaces
+{
+    public interface IChangeDefaultCustomerAddressInfoShippingAddressValidator
+        : IValidator<ChangeDefaultCustomerAddressInfoShippingAddressInput>
+    {
+    }
+}
