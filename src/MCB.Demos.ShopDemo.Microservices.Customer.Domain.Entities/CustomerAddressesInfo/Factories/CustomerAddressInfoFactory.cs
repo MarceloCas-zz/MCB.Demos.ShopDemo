15fcca9c1f437ddc0a71c252d95825733669e954
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-08-25 02:08:09 -0300

    feat: add CustomerAddressInfoFactory

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddressesInfo/Factories/CustomerAddressInfoFactory.cs
@@ -0,0 +1,28 @@
+﻿using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Factories.Interfaces;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Validators.Interfaces;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Factories
+{
+    public class CustomerAddressInfoFactory
+        : ICustomerAddressInfoFactory
+    {
+        // Fields
+        private readonly IRegisterNewCustomerAddressInfoValidator _registerNewCustomerAddressInfoValidator;
+
+        // Constructors
+        public CustomerAddressInfoFactory(
+            IRegisterNewCustomerAddressInfoValidator registerNewCustomerAddressInfoValidator
+        )
+        {
+            _registerNewCustomerAddressInfoValidator = registerNewCustomerAddressInfoValidator;
+        }
+
+        // Public Methods
+        public CustomerAddressInfo Create()
+        {
+            return new CustomerAddressInfo(
+                _registerNewCustomerAddressInfoValidator
+            );
+        }
+    }
+}
---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddressesInfo/Factories/Interfaces/ICustomerAddressInfoFactory.cs
@@ -0,0 +1,9 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Factory;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddressesInfo.Factories.Interfaces
+{
+    public interface ICustomerAddressInfoFactory
+        : IFactory<CustomerAddressInfo>
+    {
+    }
+}
