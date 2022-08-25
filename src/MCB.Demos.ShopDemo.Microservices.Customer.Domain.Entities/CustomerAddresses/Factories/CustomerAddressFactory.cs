d64aa0309cd9539291a4530afc53378eda1d0bac
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-08-25 02:04:21 -0300

    feat: add CustomerAddressFactory

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddresses/Factories/CustomerAddressFactory.cs
@@ -0,0 +1,40 @@
+﻿using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddresses.Factories.Interfaces;
+using MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddresses.Validators.Interfaces;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddresses.Factories
+{
+    public class CustomerAddressFactory
+        : ICustomerAddressFactory
+    {
+        // Fields
+        private readonly IChangeCustomerAddressTypeValidator _changeCustomerAddressTypeValidator;
+        private readonly IChangeCustomerAddressValidator _changeCustomerAddressValidator;
+        private readonly IChangeCustomerFullAddressInfoValidator _changeCustomerFullAddressInfoValidator;
+        private readonly IRegisterNewCustomerAddressValidator _registerNewCustomerAddressValidator;
+
+        // Constructors
+        public CustomerAddressFactory(
+            IChangeCustomerAddressTypeValidator changeCustomerAddressTypeValidator,
+            IChangeCustomerAddressValidator changeCustomerAddressValidator,
+            IChangeCustomerFullAddressInfoValidator changeCustomerFullAddressInfoValidator,
+            IRegisterNewCustomerAddressValidator registerNewCustomerAddressValidator
+        )
+        {
+            _changeCustomerAddressTypeValidator = changeCustomerAddressTypeValidator;
+            _changeCustomerAddressValidator = changeCustomerAddressValidator;
+            _changeCustomerFullAddressInfoValidator = changeCustomerFullAddressInfoValidator;
+            _registerNewCustomerAddressValidator = registerNewCustomerAddressValidator;
+        }
+
+        // Public Methods
+        public CustomerAddress Create()
+        {
+            return new CustomerAddress(
+                _changeCustomerAddressTypeValidator,
+                _changeCustomerAddressValidator,
+                _changeCustomerFullAddressInfoValidator,
+                _registerNewCustomerAddressValidator
+            );
+        }
+    }
+}
---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities/CustomerAddresses/Factories/Interfaces/ICustomerAddressFactory.cs
@@ -0,0 +1,9 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Factory;
+
+namespace MCB.Demos.ShopDemo.Microservices.Customer.Domain.Entities.CustomerAddresses.Factories.Interfaces
+{
+    public interface ICustomerAddressFactory
+        : IFactory<CustomerAddress>
+    {
+    }
+}
