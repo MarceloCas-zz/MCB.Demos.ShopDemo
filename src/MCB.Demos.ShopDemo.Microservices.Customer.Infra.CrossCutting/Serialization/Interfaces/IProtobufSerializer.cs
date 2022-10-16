6bbca454c76afd0c5007a58bb43652e9950c6888
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-10-15 23:02:34 -0300

    feat: add ProtobufSerializer

---
+++ src/MCB.Demos.ShopDemo.Microservices.Customer.Infra.CrossCutting/Serialization/Interfaces/IProtobufSerializer.cs
@@ -0,0 +1,7 @@
+﻿namespace MCB.Demos.ShopDemo.Microservices.Customer.Infra.CrossCutting.Serialization.Interfaces;
+
+public interface IProtobufSerializer
+{
+    byte[] SerializeToProtobuf(object obj);
+    T? DeserializeFromProtobuf<T>(byte[] byteArray);
+}
